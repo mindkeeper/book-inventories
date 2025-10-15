ab22c83f04a91da2f9995f262036ed946a1e4843
Nur Cholis Majid <nurcholismajid@192.168.1.239>
2025-10-15 17:03:57 +0700

    feat: local guard for sign in endpoint

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { CommonsModule } from 'src/commons/commons.module';
 import { UsersModule } from 'src/users/users.module';
-import { JwtStrategy } from 'src/token/strategies/jwt.strategy';
+import { JwtStrategy } from 'src/auth/strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [CommonsModule, UsersModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
 })
 export class AuthModule {}
